9d85c50332c5811dc41066c2674a604608d8cc4b
MK mac <mkmac@MKui-MacBookAir.local>
2021-11-03 16:05:57 +0900

    LoadingPage Source Code update.

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
-import {StyleSheet, Text, View, ImageBackground } from 'react-native';
-import {render} from 'react-dom';
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 
-export default class Loading extends Component{
-    render() {
-        return (
-            <ImageBackground
-                source={require("../image/MK_StartLogo.png")}
-                style = {{width:"100%", height:"100%"}}>
-                
-                </ImageBackground>
-        );
-    }
+export default class Loading extends Component {
+  render() {
+    return (
+      <ImageBackground
+        source={require("../image/MK_StartLogo.png")}
+        style={{
+            width: "100%",
+            height: 100,
+          position:'relative',
+          alignItems:'center',
+          justifyContent:'center',
+
+        }}
+      ></ImageBackground>
+    );
+  }
 }
